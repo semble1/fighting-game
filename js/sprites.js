e410f242aa0063987cec2553db0df9d8beae9611
semble1 <jaforkalam@gmail.com>
2022-06-14 23:25:36 -0400

    Overhauled playable characters

--- js/sprites.js
+++ js/sprites.js
@@ -1,11 +1,11 @@
 let knightSprites = {
     idle: {
         imageSrc: './assets/knight/knightIdle.png',
-        framesMax: 10
+        framesMax: 8
     },
     run: {
         imageSrc: './assets/knight/knightRun.png',
-        framesMax: 10
+        framesMax: 8
     },
     jump: {
         imageSrc: './assets/knight/knightJump.png',
@@ -17,70 +17,27 @@ let knightSprites = {
     },
     attack1: {
         imageSrc: './assets/knight/knightAttack1.png',
-        framesMax: 4
+        framesMax: 11
     },
     attack2: {
         imageSrc: './assets/knight/knightAttack2.png',
-        framesMax: 6
+        framesMax: 11
     },
     slide: {
         imageSrc: './assets/knight/knightSlide.png',
-        framesMax: 2
+        framesMax: 8
     },
     roll: {
         imageSrc: './assets/knight/knightRoll.png',
-        framesMax: 12
+        framesMax: 8
     },
     takeHit: {
         imageSrc: './assets/knight/knightTakeHit.png',
-        framesMax: 3
+        framesMax: 6
     },
     death: {
         imageSrc: './assets/knight/knightDeath.png',
-        framesMax: 10
-    }
-}
-
-let advSprites = {
-    idle: {
-        imageSrc: './assets/adventurer/advIdle.png',
-        framesMax: 4
-    },
-    run: {
-        imageSrc: './assets/adventurer/advRun.png',
-        framesMax: 6
-    },
-    jump: {
-        imageSrc: './assets/adventurer/advJump.png',
-        framesMax: 4
-    },
-    fall: {
-        imageSrc: './assets/adventurer/advFall.png',
-        framesMax: 2
-    },
-    attack1: {
-        imageSrc: './assets/adventurer/advAttack1.png',
-        framesMax: 5
-    },
-    attack2: {
-        imageSrc: './assets/adventurer/advAttack2.png',
-        framesMax: 6
-    },
-    slide: {
-        imageSrc: './assets/adventurer/advSlide.png',
-        framesMax: 2
-    },
-    roll: {
-        imageSrc: './assets/adventurer/advRoll.png',
-        framesMax: 4
-    },
-    takeHit: {
-        imageSrc: './assets/adventurer/advTakeHit.png',
-        framesMax: 3
-    },
-    death: {
-        imageSrc: './assets/adventurer/advDeath.png',
-        framesMax: 7
+        framesMax: 13
     }
 }
 
@@ -128,5 +85,177 @@ let rangerSprites = {
     death: {
         imageSrc: './assets/ranger/rangerDeath.png',
         framesMax: 19
+    }
+}
+
+let bladeSprites = {
+    idle: {
+        imageSrc: './assets/blade/bladeIdle.png',
+        framesMax: 8
+    },
+    run: {
+        imageSrc: './assets/blade/bladeRun.png',
+        framesMax: 8
+    },
+    jump: {
+        imageSrc: './assets/blade/bladeJump.png',
+        framesMax: 3
+    },
+    fall: {
+        imageSrc: './assets/blade/bladeFall.png',
+        framesMax: 3
+    },
+    attack1: {
+        imageSrc: './assets/blade/bladeAttack1.png',
+        framesMax: 6
+    },
+    attack2: {
+        imageSrc: './assets/blade/bladeAttack2.png',
+        framesMax: 5
+    },
+    slide: {
+        imageSrc: './assets/blade/bladeSlide.png',
+        framesMax: 8
+    },
+    roll: {
+        imageSrc: './assets/blade/bladeRoll.png',
+        framesMax: 7
+    },
+    takeHit: {
+        imageSrc: './assets/blade/bladeTakeHit.png',
+        framesMax: 6
+    },
+    death: {
+        imageSrc: './assets/blade/bladeDeath.png',
+        framesMax: 12
+    }
+}
+
+let priestSprites = {
+    idle: {
+        imageSrc: './assets/priest/priestIdle.png',
+        framesMax: 8
+    },
+    run: {
+        imageSrc: './assets/priest/priestRun.png',
+        framesMax: 10
+    },
+    jump: {
+        imageSrc: './assets/priest/priestJump.png',
+        framesMax: 3
+    },
+    fall: {
+        imageSrc: './assets/priest/priestFall.png',
+        framesMax: 3
+    },
+    attack1: {
+        imageSrc: './assets/priest/priestAttack1.png',
+        framesMax: 7
+    },
+    attack2: {
+        imageSrc: './assets/priest/priestAttack2.png',
+        framesMax: 17
+    },
+    slide: {
+        imageSrc: './assets/priest/priestSlide.png',
+        framesMax: 8
+    },
+    roll: {
+        imageSrc: './assets/priest/priestRoll.png',
+        framesMax: 6
+    },
+    takeHit: {
+        imageSrc: './assets/priest/priestTakeHit.png',
+        framesMax: 7
+    },
+    death: {
+        imageSrc: './assets/priest/priestDeath.png',
+        framesMax: 16
+    }
+}
+
+let shinSprites = {
+    idle: {
+        imageSrc: './assets/shin/shinIdle.png',
+        framesMax: 8
+    },
+    run: {
+        imageSrc: './assets/shin/shinRun.png',
+        framesMax: 8
+    },
+    jump: {
+        imageSrc: './assets/shin/shinJump.png',
+        framesMax: 3
+    },
+    fall: {
+        imageSrc: './assets/shin/shinFall.png',
+        framesMax: 3
+    },
+    attack1: {
+        imageSrc: './assets/shin/shinAttack1.png',
+        framesMax: 8
+    },
+    attack2: {
+        imageSrc: './assets/shin/shinAttack2.png',
+        framesMax: 14
+    },
+    slide: {
+        imageSrc: './assets/shin/shinSlide.png',
+        framesMax: 8
+    },
+    roll: {
+        imageSrc: './assets/shin/shinRoll.png',
+        framesMax: 6
+    },
+    takeHit: {
+        imageSrc: './assets/shin/shinTakeHit.png',
+        framesMax: 6
+    },
+    death: {
+        imageSrc: './assets/shin/shinDeath.png',
+        framesMax: 19
+    }
+}
+
+let monkSprites = {
+    idle: {
+        imageSrc: './assets/monk/monkIdle.png',
+        framesMax: 6
+    },
+    run: {
+        imageSrc: './assets/monk/monkRun.png',
+        framesMax: 8
+    },
+    jump: {
+        imageSrc: './assets/monk/monkJump.png',
+        framesMax: 3
+    },
+    fall: {
+        imageSrc: './assets/monk/monkFall.png',
+        framesMax: 3
+    },
+    attack1: {
+        imageSrc: './assets/monk/monkAttack1.png',
+        framesMax: 6
+    },
+    attack2: {
+        imageSrc: './assets/monk/monkAttack2.png',
+        framesMax: 7
+    },
+    slide: {
+        imageSrc: './assets/monk/monkSlide.png',
+        framesMax: 8
+    },
+    roll: {
+        imageSrc: './assets/monk/monkRoll.png',
+        framesMax: 6
+    },
+    takeHit: {
+        imageSrc: './assets/monk/monkTakeHit.png',
+        framesMax: 6
+    },
+    death: {
+        imageSrc: './assets/monk/monkDeath.png',
+        framesMax: 18
     }
 }
--- js/switch.js
+++ js/switch.js
@@ -1,40 +1,24 @@
 const characters = document.querySelector('.characters')
 const knight = document.querySelector('.knight')
-const adv = document.querySelector('.adv')
+const blade = document.querySelector('.blade')
 const ranger = document.querySelector('.ranger')
+const priest = document.querySelector('.priest')
+const shin = document.querySelector('.shin')
+const monk = document.querySelector('.monk')
 
 knight.onclick = function() {
-    player.scale = 3
-    player.framesHold = 3
+    player.scale = 2.5
+    player.framesHold = 5
     player.offset = {
         x: 130,
-        y: 100
+        y: 177
     }
     player.attackBox.width = 115
+    player.offset.x = 300
     player.sprites = knightSprites
     for (const sprite in player.sprites) {
         knightSprites[sprite].image = new Image()
         knightSprites[sprite].image.src = knightSprites[sprite].imageSrc
-    }
-    characters.remove()
-    animate()
-}
-
-adv.onclick = function() {
-    player.scale = 3.5
-    player.framesHold = 5
-    player.offset = {
-        x: 130,
-        y: -12
-    }
-    player.attackBox.width = 35
-    player.attackBox.offset.x = 50
-    player.offset.x = 30
-    player.position.x = 100
-    player.sprites = advSprites
-    for (const sprite in player.sprites) {
-        advSprites[sprite].image = new Image()
-        advSprites[sprite].image.src = advSprites[sprite].imageSrc
     }
     characters.remove()
     animate()
@@ -45,7 +29,7 @@ ranger.onclick = function() {
     player.framesHold = 5
     player.offset = {
         x: 130,
-        y: 175
+        y: 177
     }
     player.attackBox.width = 290
     player.attackBox.offset.x = 100
@@ -54,6 +38,82 @@ ranger.onclick = function() {
     for (const sprite in player.sprites) {
         rangerSprites[sprite].image = new Image()
         rangerSprites[sprite].image.src = rangerSprites[sprite].imageSrc
+    }
+    characters.remove()
+    animate()
+}
+
+blade.onclick = function() {
+    player.scale = 2.55
+    player.framesHold = 5
+    player.offset = {
+        x: 130,
+        y: 183
+    }
+    player.attackBox.width = 290
+    player.attackBox.offset.x = 100
+    player.offset.x = 300
+    player.sprites = bladeSprites
+    for (const sprite in player.sprites) {
+        bladeSprites[sprite].image = new Image()
+        bladeSprites[sprite].image.src = bladeSprites[sprite].imageSrc
+    }
+    characters.remove()
+    animate()
+}
+
+priest.onclick = function() {
+    player.scale = 2.6
+    player.framesHold = 5
+    player.offset = {
+        x: 130,
+        y: 190
+    }
+    player.attackBox.width = 290
+    player.attackBox.offset.x = 100
+    player.offset.x = 300
+    player.sprites = priestSprites
+    for (const sprite in player.sprites) {
+        priestSprites[sprite].image = new Image()
+        priestSprites[sprite].image.src = priestSprites[sprite].imageSrc
+    }
+    characters.remove()
+    animate()
+}
+
+shin.onclick = function() {
+    player.scale = 2.6
+    player.framesHold = 5
+    player.offset = {
+        x: 130,
+        y: 190
+    }
+    player.attackBox.width = 290
+    player.attackBox.offset.x = 100
+    player.offset.x = 300
+    player.sprites = shinSprites
+    for (const sprite in player.sprites) {
+        shinSprites[sprite].image = new Image()
+        shinSprites[sprite].image.src = shinSprites[sprite].imageSrc
+    }
+    characters.remove()
+    animate()
+}
+
+monk.onclick = function() {
+    player.scale = 3.1
+    player.framesHold = 5
+    player.offset = {
+        x: 130,
+        y: 235
+    }
+    player.attackBox.width = 290
+    player.attackBox.offset.x = 100
+    player.offset.x = 400
+    player.sprites = monkSprites
+    for (const sprite in player.sprites) {
+        monkSprites[sprite].image = new Image()
+        monkSprites[sprite].image.src = monkSprites[sprite].imageSrc
     }
     characters.remove()
     animate()
